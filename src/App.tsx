36835bf7c7801b43b3a518797217b0c78f936ce2
tawan123456789 <tawan.psm63@gmail.com>
2026-01-11 12:15:44 +0700

    reverse proxie

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ type Todo = {
   completed: boolean;
 };
 
-const API_BASE = import.meta.env.VITE_API_BASE ?? "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_BASE ?? "/api";
 
 type Styles = {
   page: React.CSSProperties;
